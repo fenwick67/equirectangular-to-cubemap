3ec0ed9cdbb1916c4b09583f2b6dbf04647bd680
Drew Harwell <fenwick67@users.noreply.github.com>
2017-09-20 16:27:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 var convertToFaces = require('threejs-cubemap-to-equirectangular').transformToCubeFaces;
 var argv = require('yargs').argv;
 var filename = argv._[0];
+var imageSize = require('image-size');
+var sharp = require('sharp');
 
 // read file
-var inFile = fs.readFileSync(filename,'buffer');
-
-// convert the file to image data
-var imageData = isjpeg(inFile)?readJpeg(inFile):readPng(inFile);
-var size = imageSize(inFile).height; 
-
-var r = new Array(6);
-for (var i = 0; i < 6; i ++){
-  r[i] = {
-    width:size,
-    height:size,
-    data:new Array(4*size*size)
+sharp(filename).raw().toBuffer(function(er,pixelData,info){
+  if (er){
+    throw er
   }
-}
 
-// convert to cube faces
-var faces = convertToFaces({data:imageData},r);
+  // convert the file to image data
+  var size = info.height; 
 
-console.log(faces);
+  var r = new Array(6);
+  for (var i = 0; i < 6; i ++){
+    r[i] = {
+      width:size,
+      height:size,
+      data:new Array(4*size*size)
+    }
+  }
+
+  // convert to cube faces
+  var faces = convertToFaces({width:size*2,height:size,data:imageData},r);
+
+  console.log(faces);
+
+})
